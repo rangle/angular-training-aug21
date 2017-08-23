8372807f513da2933b708760d2eb1593dfd01331
Varun Vachhar <varunvachhar@gmail.com>
2017-08-23 12:03:46 -0400

    post route

--- src/app/post-container/post-container.component.ts
+++ src/app/post-container/post-container.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PostsService } from '../posts.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-post-container',
@@ -7,14 +9,32 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./post-container.component.css']
 })
 export class PostContainerComponent implements OnInit {
-  id = null;
-  constructor(private route: ActivatedRoute) { }
+  post;
+
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService
+  ) { }
 
   ngOnInit() {
-    this.route.params
-      .subscribe((params) => {
-        this.id = +params.id;
-      });
+    const id$ = this.route.params
+      .pluck('id')
+      .map(id => +id);
+
+    Observable.combineLatest(
+      id$,
+      this.postsService.posts$
+    )
+    .map(this.findPost)
+    .subscribe((post) => {
+      console.log(post);
+      this.post = post;
+    });
+
+  }
+
+  findPost([id, posts]) {
+    return posts.find(post => post.id === id);
   }
 
 }
